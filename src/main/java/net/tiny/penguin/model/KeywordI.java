e58364e7fa2bf24530dfeb2aa7e508ea100a2542
Sander <sander.bartholomees@gmail.com>
2014-03-15 03:59:35 +0100

    Add id annotation to interface

--- src/main/java/net/tiny/penguin/model/KeywordI.java
+++ src/main/java/net/tiny/penguin/model/KeywordI.java
@@ -1,8 +1,11 @@
 package net.tiny.penguin.model;
 
 
+import org.springframework.data.annotation.Id;
+
 public interface KeywordI {
 
+    @Id
     public String getId();
 
     public String getKeyword();
